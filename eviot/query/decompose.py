00cc88c06ef9486ac45368c03d974735f562b970
Sabharish <sabharishhhpv2304@gmail.com>
2026-03-04 14:13:06 +0530

    spacy fix

--- eviot/query/decompose.py
+++ eviot/query/decompose.py
@@ -2,7 +2,14 @@ import spacy
 import torch
 from eviot.encoders.encoder import Encoder
 
-nlp = spacy.load("en_core_web_sm")
+import spacy
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 encoder = Encoder()
 
 STOPWORDS = {
